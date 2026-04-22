56ee32ef1e709b8b6841870c53a73e285c68b6c7
René Verheij <email@rene.life>
2026-04-22 22:37:43 +0100

    set up single-branch publish workflow (via 'linked setup-publish')

---
+++ .changeset/initial-release.md
@@ -0,0 +1,5 @@
+---
+'@_linked/ui': patch
+---
+
+Initial release under the new publishing setup.
--- lib/esm/ontologies/lincd-ui.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const loadData: () => Promise<any>;
-export declare const ns: (term: string) => import("@_linked/core/utils/NodeReference.js").NodeReferenceValue;
-export declare const _self: import("@_linked/core/utils/NodeReference.js").NodeReferenceValue;
-export declare const lincdUi: {
-    _self: import("@_linked/core/utils/NodeReference.js").NodeReferenceValue;
-};
